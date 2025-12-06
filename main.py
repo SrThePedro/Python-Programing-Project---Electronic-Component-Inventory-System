5470c0dfa419b06053f1490251248445491b5ec1
slhemnkr <slhemnkr@gmail.com>
2025-12-06 12:47:28 +0300

    add Resistor and Capacitor

--- main.py
+++ main.py
@@ -43,4 +43,47 @@ class Component:
         else:
             raise TypeError
 
-class
+class Resistor(Component):
+    def __init__(self,name,id,price,number,resistance_value,power_rating):
+        super().__init__(name,id,price,number)
+        self.resistance_value = resistance_value
+        self.power_rating = power_rating
+
+    @property
+    def resistance_value(self):
+        return self.resistance_value
+
+    @resistance_value.setter
+    def resistance_value(self, value):
+        self.resistance_value = value
+
+    @property
+    def power_rating(self):
+        return self.power_rating
+
+    @power_rating.setter
+    def power_rating(self,value):
+        self.power_rating = value
+
+    def __str__(self):
+        return f"{self.id} {self.name} {self.price} {self.number} {self.resistance_value} {self.power_rating}"
+
+class Capacitor(Component):
+    def __init__(self,name,id,price,number,capacitance,voltage_rating):
+        super().__init__(name,id,price,number)
+        self.capacitance = capacitance
+        self.voltage_rating = voltage_rating
+
+    @property
+    def capacitance(self):
+        return self.capacitance
+
+    @capacitance.setter
+    def capacitance(self,value):
+        self.capacitance = value
+
+    def __str__(self):
+        return f"{self.id} {self.name} {self.price} {self.number} {self.capacitance} {self.voltage_rating}"
+
+
+
