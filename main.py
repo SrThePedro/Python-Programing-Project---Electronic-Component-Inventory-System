fbcb791b758efdc7751dc03bc9c89b80303347e2
slhemnkr <slhemnkr@gmail.com>
2025-12-06 12:18:33 +0300

    create main component class

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
-print("hello world")
-print("hello world")
-print("hello world")
+class Component:
+    def __init__(self, name, id ,price, number ):
+        self.name = name
+        self.id = id
+        self.price = price
+        self.number = number
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value):
+        self.name = value
+
+    @property
+    def id(self):
+        return self.id
+
+    @id.setter
+    def id(self, value):
+        self.id = value
+
+    @property
+    def price(self):
+        return self.price
+
+    @price.setter
+    def price(self, value):
+        if type(value) == int and value >= 0:
+            self.price = value
+        else:
+            raise TypeError
+
+    @property
+    def number(self):
+        return self.number
+
+    @number.setter
+    def number(self, value):
+        if type(value) == int:
+            self.number = value
+        else:
+            raise TypeError
+
+class
